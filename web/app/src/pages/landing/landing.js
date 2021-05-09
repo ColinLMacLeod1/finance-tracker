ec0dd9068e84fc857f0148cf5a9dff7331e0bd04
Colin MacLeod <colinlmacleod1@gmail.com>
2021-05-09 15:53:40 -0400

    added state to the landing

--- web/app/src/pages/landing/landing.js
+++ web/app/src/pages/landing/landing.js
@@ -1,13 +1,20 @@
 import './index.css';
+import { useState } from 'react';
 import logo from './img/logo.svg';
 
 function Landing() {
+  const [ state, setState ] = useState(2);
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+
+          State: {state}
         </p>
         <a
           className="App-link"
@@ -15,8 +22,9 @@ function Landing() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn sdakfjsakjdhfReact
         </a>
+        <button onClick={() => {setState(5);}}> Click me! </button>
       </header>
     </div>
   );
